2057d3736b5c75a8fc6736974790602e958fdd9e
PadraigHawkins <107004777+PadraigHawkins@users.noreply.github.com>
2022-11-05 12:22:56 +0000

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,7 +1,23 @@
 function GetValue()
 {
-    var myarray= new Array("PYG","THE LONG HAUL","DEVITTS","HOGANS","GROGANS","THE CAMDEN","NO NAME BAR","CASSIDYS","MARY'S BAR",
-    "The Gravediggers","The Cobblestone","Whelan's","P.Mac's","Flannery's","Market Bar","The Bernard Shaw","Jimmy Rabbitte's");
+    var myarray= new Array(    
+    "PYG",
+    "THE LONG HAUL",
+    "DEVITTS",
+    "HOGANS",
+    "GROGANS",
+    "THE CAMDEN",
+    "NO NAME BAR",
+    "CASSIDYS",
+    "MARY'S BAR",
+    "THE GRAVEDIGGERS",
+    "THECOBBLESTONE",
+    "WHELAN'S",
+    "P.MAC’S",
+    "FLANNERY'S",
+    "MARKET BAR",
+    "THE BERNARD SHAW",
+    "JIMMY RABBITTES");
     var random = myarray[Math.floor(Math.random() * myarray.length)];
    document.getElementById("message").innerHTML=random;
 }
